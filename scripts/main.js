bbb5b4502c49fdd383c7d27475d173ac480b3cd4
matthew-woodington <smwoodington@gmail.com>
2022-09-13 15:21:55 -0400

    adds answer for qs 2-4

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,34 @@ const prices = items.map((num) => {
 const averagePrice = prices.reduce((acc, i) => acc + i) / prices.length;
 
 console.log(`The average price is $${averagePrice.toFixed(2)}`);
+
+// 2. Show me how to get an array of items that cost between $14.00 and $18.00 USD. Please console.log the answer.
+
+const specificCostList = items.filter((product) => {
+  return product.price <= 18 && product.price >= 14;
+});
+
+console.log(
+  `Items that cost between $14.00 USD and $18.00 USD:`,
+  specificCostList
+);
+
+// 3. Show me how find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
+
+const alternateCurrency = items.filter((item) => {
+  return item["currency_code"] === "GBP";
+});
+
+// console.log(alternateCurrency);
+
+alternateCurrency.forEach((item) =>
+  console.log(`${item.title} costs £${item.price}`)
+);
+
+// 4. Show me how to find which items are made of wood. Please console.log the ones you find.
+
+const madeOfWood = items.filter((item) => {
+  return item["materials"].includes("wood");
+});
+
+console.log(`Items that are made of wood include:`, madeOfWood);
